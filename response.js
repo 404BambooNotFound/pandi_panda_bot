c4c17227377ba93fa80f162de56db2dc33dfa259
Elercia <rabierv@hotmail.fr>
2018-12-07 01:26:36 +0100

    Add program core

---
+++ response.js
@@ -0,0 +1,26 @@
+let responses = [
+    {
+        "keyword": ["Bonjour", "hello"],
+        "responses": {
+            "default": ["reponse"],
+            "angry": ["Panda this bambou"],
+            "happy": ["I'm so panda"]
+        }
+    },
+    {
+        "keyword": ["au revoir", "bitre"],
+        "responses": {
+            "default": ["deded"],
+            "angry": ["dede"],
+            "happy": ["deded"]
+        }
+    },
+    {
+        "keyword": undefined,
+        "responses": {
+            "default": ["ffefef"],
+            "angry": ["efefefe"],
+            "happy": ["efefefefe"]
+        }
+    }
+];
